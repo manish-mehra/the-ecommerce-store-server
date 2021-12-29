bd7976b0cdb3255cc73c348784b098016d5ca358
manish-mehra <manishmehra1469@gmail.com>
2021-12-29 18:05:58 +0530

    added auth: register & login

---
+++ controllers/authController.js
@@ -0,0 +1,53 @@
+const User = require('../models/User')
+const {StatusCodes} = require('http-status-codes')
+const CustomError = require('../errors')
+const {attachCookiesToResponse, createTokenUser} = require('../utils')
+const jwt = require('jsonwebtoken')
+const login = async(req, res)=>{
+    const {email, password} = req.body
+    if(!email || !password){
+        throw new CustomError.BadRequestError('Please provide email and password')
+    }
+    const user = await User.findOne({email})
+    if(!user){
+        throw new CustomError.UnauthenticatedError('Invalid Credentials')
+    }
+
+    const ifPasswordExist= await user.comparePassword(password)
+    if(!ifPasswordExist){
+        throw new CustomError.UnauthenticatedError('Invalid Credentials')
+    }
+    const tokenUser = createTokenUser(user)
+    attachCookiesToResponse({res, user: tokenUser})
+    res.status(StatusCodes.OK).json({user: tokenUser})
+}
+
+const register = async (req, res)=>{
+
+    const {name, email, password} = req.body
+
+    const emailAlreadyExist = await User.findOne({email})
+
+    if(emailAlreadyExist){
+        throw new CustomError.BadRequestError('Email already exists') 
+    }
+
+    //first registered user is an admin
+    // const isFirstAccount = (await User.countDocuments({})) === 0
+    // const role = isFirstAccount? 'admin': 'user'
+
+    const user = await User.create({
+        name, email, password, role: 'user'
+    })
+    
+    const tokenUser = createTokenUser(user)
+    attachCookiesToResponse({res, user: tokenUser})
+    res.status(StatusCodes.CREATED).json({user: tokenUser})
+    res.status(StatusCodes.CREATED).send(user)
+}
+
+const logout = async (req, res)=>{
+
+}
+
+module.exports = {login, register, logout}
--- server.js
+++ server.js
@@ -3,10 +3,31 @@ require('express-async-errors')
 
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const cors = require('cors');
+const cookieParser = require('cookie-parser')
 //import db
 const connectDB = require('./db/connect')
 
+//router
+const authRouter = require('./routes/authRoutes')
+
+//middleware
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+
+app.use(cors())
 app.use(express.json())
+app.use(cookieParser(process.env.JWT_SECRET))
+//router
+app.use('/api/v1/auth', authRouter)
+
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+
 
 const port = process.env.PORT || 5000
 const start = async()=>{
