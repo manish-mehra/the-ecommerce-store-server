003cf982766f236de9d9f0b3092531c15ac2e910
manish-mehra <manishmehra1469@gmail.com>
2022-01-04 19:01:23 +0530

    try: fixing authorization

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,56 +1,54 @@
-const User = require('../models/User')
-const {StatusCodes} = require('http-status-codes')
-const CustomError = require('../errors')
-const {attachCookiesToResponse, createTokenUser} = require('../utils')
-const jwt = require('jsonwebtoken')
-const login = async(req, res)=>{
-    const {email, password} = req.body
-    if(!email || !password){
-        throw new CustomError.BadRequestError('Please provide email and password')
-    }
-    const user = await User.findOne({email})
-    if(!user){
-        throw new CustomError.UnauthenticatedError('Invalid Credentials')
-    }
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+const { attachCookiesToResponse, createTokenUser } = require('../utils');
 
-    const ifPasswordExist= await user.comparePassword(password)
-    if(!ifPasswordExist){
-        throw new CustomError.UnauthenticatedError('Invalid Credentials')
-    }
-    const tokenUser = createTokenUser(user)
-    attachCookiesToResponse({res, user: tokenUser})
-    res.status(StatusCodes.OK).json({user: tokenUser})
-}
+const register = async (req, res) => {
+  const { email, name, password } = req.body;
 
-const register = async (req, res)=>{
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError('Email already exists');
+  }
 
-    const {name, email, password} = req.body
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? 'admin' : 'user';
 
-    const emailAlreadyExist = await User.findOne({email})
+  const user = await User.create({ name, email, password, role });
+  const tokenUser = createTokenUser(user);
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
+};
+const login = async (req, res) => {
+  const { email, password } = req.body;
 
-    if(emailAlreadyExist){
-        throw new CustomError.BadRequestError('Email already exists') 
-    }
+  if (!email || !password) {
+    throw new CustomError.BadRequestError('Please provide email and password');
+  }
+  const user = await User.findOne({ email });
 
-    //first registered user is an admin
-    // const isFirstAccount = (await User.countDocuments({})) === 0
-    // const role = isFirstAccount? 'admin': 'user'
+  if (!user) {
+    throw new CustomError.UnauthenticatedError('Invalid Credentials');
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError('Invalid Credentials');
+  }
+  const tokenUser = createTokenUser(user);
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.OK).json({ user: tokenUser });
+};
+const logout = async (req, res) => {
+  res.cookie('token', 'logout', {
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000),
+  });
+  res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
+};
 
-    const user = await User.create({
-        name, email, password, role: 'user'
-    })
-    
-    const tokenUser = createTokenUser(user)
-    attachCookiesToResponse({res, user: tokenUser})
-    res.status(StatusCodes.CREATED).json({user: tokenUser})
-}
-
-const logout = async (req, res)=>{
-    res.cookie('token', 'logout', {
-        httpOnly: true,
-        expires: new Date(Date.now()+ 1000)
-    })
-    res.status(StatusCodes.OK).json({msg: 'user logged out!'})
-}
-
-module.exports = {login, register, logout}
+module.exports = {
+  register,
+  login,
+  logout,
+};
