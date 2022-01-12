9adad8cc7551216865c7456ba178d13774a6eef2
manish-mehra <manishmehra1469@gmail.com>
2022-01-12 19:13:46 +0530

    feat: review controller and routes

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const connectDB = require('./db/connect')
 const authRouter = require('./routes/authRoutes')
 const userRouter = require('./routes/userRoutes')
 const productRouter = require('./routes/productRoutes')
+const reviewRouter = require('./routes/reviewRoutes')
 //middleware
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
@@ -25,6 +26,8 @@ app.use(cookieParser(process.env.JWT_SECRET))
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/products', productRouter)
+app.use('/api/v1/reviews', reviewRouter)
+
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
