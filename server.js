3d990799231894e38fb5230ecd7b0ffd02453919
manish-mehra <manishmehra1469@gmail.com>
2022-01-17 16:45:44 +0530

    chore: added security packages

--- server.js
+++ server.js
@@ -1,50 +1,70 @@
-require('dotenv').config()
-require('express-async-errors')
+require('dotenv').config();
+require('express-async-errors');
+// express
 
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
+const express = require('express');
+const app = express();
+// rest of the packages
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 const cors = require('cors');
-const cookieParser = require('cookie-parser')
-//import db
-const connectDB = require('./db/connect')
+const mongoSanitize = require('express-mongo-sanitize');
 
-//router
-const authRouter = require('./routes/authRoutes')
-const userRouter = require('./routes/userRoutes')
-const productRouter = require('./routes/productRoutes')
-const reviewRouter = require('./routes/reviewRoutes')
-const orderRouter = require('./routes/orderRoutes')
-//middleware
-const notFoundMiddleware = require('./middleware/not-found')
-const errorHandlerMiddleware = require('./middleware/error-handler')
+// database
+const connectDB = require('./db/connect');
 
+//  routers
+const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const orderRouter = require('./routes/orderRoutes');
 
-app.use(cors())
-app.use(express.json())
-app.use(cookieParser(process.env.JWT_SECRET))
-//router
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/users', userRouter)
-app.use('/api/v1/products', productRouter)
-app.use('/api/v1/reviews', reviewRouter)
-app.use('/api/v1/orders', orderRouter)
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
 
-const port = process.env.PORT || 5000
-const start = async()=>{
-    try {
-        await connectDB(process.env.MONGO_URI)
-        console.log('connected to mongodb...')
-        app.listen(port, ()=>{
-            console.log(`Server running at port ${port}`)
-        })
-    } catch (error) {
-        console.log(error)
-    }
-}
+app.use(express.static('./public'));
+app.use(fileUpload());
 
-start()
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
+app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/orders', orderRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
